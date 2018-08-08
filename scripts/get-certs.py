097538462a22020525177b31a8132d2bfe9236b9
Alexey Sakhnov <sakhnov@nagradion.ru>
2018-08-08 13:04:13 +0300

    update get-certs command

--- scripts/get-certs.py
+++ scripts/get-certs.py
@@ -6,6 +6,6 @@ if "HTTPS_DOMAINS" in environ:
     for domain_host in environ["HTTPS_DOMAINS"].split(";"):
         parts = domain_host.split(":")
         print "Getting cert for {domain}".format(domain=parts[0])
-        call(["certbot", "certonly", "--register-unsafely-without-email", "--webroot", "-w",  "/var/www", "-d", parts[0] ])
+        call(["certbot", "certonly", "--keep", ""--register-unsafely-without-email", "--webroot", "-w",  "/var/www", "-d", parts[0] ])
 else:
     print "HTTPS_DOMAINS env not defined"
