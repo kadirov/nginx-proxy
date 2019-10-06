1b73cf4c1da491c3494e9f71b126d895c3c2620c
Alexey Sakhnov <sakhnov@nagradion.ru>
2019-10-06 17:30:35 +0300

    STATS-938 Более удобный файл конфигурации и прокси HTTPS-запросов на HTTP порт

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 Nginx reverse proxy docker container for app deploying
 
+Конфигурационный файл размещается в `config/config.txt`. За основу можно взять `config/config.example.txt`
+
 ### Добавить сертификат для домена
 
 ```bash
-docker-compose run certbot /scripts/get-certs.py
+docker-compose run certbot /scripts/get-certs
 ```
--- scripts/generate-config.sh
+++ scripts/generate-config.sh
@@ -1,30 +1,41 @@
 #!/usr/bin/env bash
 
 
-TARGET="/etc/nginx/conf.d"
+TARGET="./etc/nginx/conf.d"
 CERT_ROOT="/etc/letsencrypt/live"
+CONFIG_FILE="../config.example.txt"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-TPL_HTTPS="$DIR/https.conf";
-TPL_HTTP="$DIR/http.conf";
+TPL_HTTPS="$DIR/https.conf"
+TPL_HTTP="$DIR/http.conf"
 
 rm ${TARGET}/*
 
-IFS=';' read -ra ADDR <<< "$HTTP_DOMAINS"
-for i in "${ADDR[@]}"; do
-    IFS=: read -r DOMAIN PORT <<< "$i"
-    sed -e "s/{DOMAIN}/$DOMAIN/g; s/{PORT}/$PORT/g" "$TPL_HTTP" >> "$TARGET/$DOMAIN.conf"
-    echo "Create HTTP config for $DOMAIN"
-done
+while read -r DOMAIN HTTP_PORT HTTPS_PORT
+do
 
-IFS=';' read -ra ADDR <<< "$HTTPS_DOMAINS"
-for i in "${ADDR[@]}"; do
-    IFS=: read -r DOMAIN PORT <<< "$i"
-    CERT_DIR="$CERT_ROOT/$DOMAIN"
-    if [ -d ${CERT_DIR} ]; then
-        sed -e "s/{DOMAIN}/$DOMAIN/g; s/{PORT}/$PORT/g" "$TPL_HTTPS" >> "$TARGET/$DOMAIN.conf"
-        echo "Create HTTPS config for $DOMAIN"
-    else
-        echo "Skip HTTPS config for $DOMAIN because cert dir not found: ${CERT_DIR}"
-    fi
-done
+  if [[ "x$DOMAIN" = "x#" ]] || [[ "x$DOMAIN" = "x" ]]; then
+    continue
+  fi
+
+  printf "%s %s %s\n" $DOMAIN $HTTP_PORT $HTTPS_PORT
+
+  sed -e "s/{DOMAIN}/$DOMAIN/g; s/{PORT}/$HTTP_PORT/g" "$TPL_HTTP" >> "$TARGET/$DOMAIN.conf"
+  echo "Create HTTP config for $DOMAIN"
+
+  if [[ "$HTTPS_PORT" = "" ]]; then
+    continue
+  fi
+
+  CERT_DIR="$CERT_ROOT/$DOMAIN"
+  [[ -d ${CERT_DIR} ]] || echo "Skip HTTPS config for $DOMAIN because cert dir not found: ${CERT_DIR}" && continue
+
+  case "$HTTPS_PORT" in
+    "~") HTTPS_PORT=$HTTP_PORT PROTOCOL=http ;;
+    *) PROTOCOL=https ;;
+  esac
+
+  sed -e "s/{DOMAIN}/$DOMAIN/g; s/{PORT}/$HTTPS_PORT/g; s/{PROTOCOL}/$PROTOCOL/g" "$TPL_HTTPS" >> "$TARGET/$DOMAIN.conf"
+  echo "Create HTTPS config for $DOMAIN"
+
+done <"$CONFIG_FILE"
