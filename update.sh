456660a2480e4860ec61bb3072f1db172d530fa2
Akmal Kadirov <akmal@kadirov.org>
2023-06-13 20:08:49 +0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ echo "Updating certbot certificates..."
 
 DOCKERFILE=/root/nginx-proxy/docker-compose.yml
 
-docker-compose -f $DOCKERFILE run certbot /scripts/get-certs
-docker-compose -f $DOCKERFILE down
-docker-compose -f $DOCKERFILE up -d
+docker compose -f $DOCKERFILE run certbot /scripts/get-certs
+docker compose -f $DOCKERFILE down
+docker compose -f $DOCKERFILE up -d
