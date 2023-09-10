14fd6c3af436abcccd19e8d43ae9c801e18bf423
acuere <acuere@github.com>
2023-09-11 02:29:27 +0500

    Add autorun crontab 15th of every month

--- update.sh
+++ update.sh
@@ -7,3 +7,5 @@ DOCKERFILE=/root/nginx-proxy/docker-compose.yml
 docker compose -f $DOCKERFILE run certbot /scripts/get-certs
 docker compose -f $DOCKERFILE down
 docker compose -f $DOCKERFILE up -d
+
+./autorun.sh
