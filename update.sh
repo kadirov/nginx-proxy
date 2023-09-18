782552d5bfd4b63278e21b09ece690373b53bf66
acuere <acuere@github.com>
2023-09-18 12:07:43 +0500

    make autorun.sh executable  and add REPO_DIR va

--- update.sh
+++ update.sh
@@ -3,9 +3,10 @@
 echo "Updating certbot certificates..."
 
 DOCKERFILE=/root/nginx-proxy/docker-compose.yml
+REPO_DIR=/root/nginx-proxy
 
 docker compose -f $DOCKERFILE run certbot /scripts/get-certs
 docker compose -f $DOCKERFILE down
 docker compose -f $DOCKERFILE up -d
 
-./autorun.sh
+/bin/bash $REPO_DIR/autorun.sh
