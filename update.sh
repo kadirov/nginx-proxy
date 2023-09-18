6ab459ad4cdd3ca53b6fd9e3c45cbff409f12384
acuere <acuere@github.com>
2023-09-18 22:47:11 +0500

    rename autorun to setup-cron.sh

--- update.sh
+++ update.sh
@@ -9,4 +9,4 @@ docker compose -f $DOCKERFILE run certbot /scripts/get-certs
 docker compose -f $DOCKERFILE down
 docker compose -f $DOCKERFILE up -d
 
-/bin/bash $REPO_DIR/autorun.sh
+/bin/bash $REPO_DIR/setup-cron.sh
